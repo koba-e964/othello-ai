992694f85a2dc8aed94fbccb2104f75f06478b2a
koba-e964 <koba-e964@kobae964-Latitude-E5410.(none)>
2014-07-25 23:27:02 +0900

    Add functions for CBoard
    Constant version of Board (two Int64's)

--- Play.hs
+++ Play.hs
@@ -1,11 +1,13 @@
 module Play where 
 
+import Control.Monad
+import Control.Monad.ST
 import Data.Bits
 import Data.Int
 import Data.Array.IO
 import Data.Array.MArray
 import Data.List ((\\))
-import Control.Monad 
+import Data.STRef
 
 import System.Random
 import Text.Printf
@@ -34,7 +36,7 @@ initCBoard = CBoard (2^27 + 2^36) (2^28 + 2^35)
 
 readCBoard :: CBoard -> Int -> Int -> Color
 readCBoard board i j
-  | i <= 0 || i >= 9 || j <= 0 || j >= 9 = error $ printf "out of board: (%d, %d)" i j
+  | i <= 0 || i >= 9 || j <= 0 || j >= 9 = sentinel
   | otherwise                            = readCBoardUnsafe board i j
 
 readCBoardUnsafe :: CBoard -> Int -> Int -> Color
@@ -46,6 +48,69 @@ readCBoardUnsafe board@(CBoard bl wh) i j =
     in
      if bbit /= 0 then black else if wbit /= 0 then white else none
   
+isValidMoveC :: CBoard -> Color -> (Int,Int) -> Bool 
+isValidMoveC board color (i,j) =
+    let e = readCBoard board i j in
+       if e == none then 
+           isEffectiveC board color (i,j)
+       else
+           False 
+
+isEffectiveC :: CBoard -> Color -> (Int,Int) -> Bool
+isEffectiveC board color (i,j) = 
+    let ms = flippableIndicesC board color (i,j) in
+    not $ null ms
+
+flippableIndicesC :: CBoard -> Color -> (Int,Int) -> [(Int,Int)]
+flippableIndicesC board color (i,j) = 
+    concatMap (\(di,dj) -> flippableIndicesLineC board color (di,dj) (i+di,j+dj)) dirs
+
+flippableIndicesLineC :: CBoard -> Color -> (Int,Int) -> (Int,Int) -> [(Int,Int)]
+flippableIndicesLineC board color (di,dj) (i,j) =
+    checkLine (di,dj) (i,j) []
+    where                    
+      ocolor = oppositeColor color
+      checkLine (di,dj) (i,j) r =
+          let c = readCBoard board i j in
+             if c == ocolor then 
+                 checkLine' (di,dj) (i+di,j+dj) ( (i,j) : r )
+             else 
+                 []
+      checkLine' (di,dj) (i,j) r =
+          let c = readCBoard board i j in
+             if c == ocolor then 
+                 checkLine' (di,dj) (i+di,j+dj) ( (i,j) : r )
+             else if c == color then 
+                      r 
+                  else
+                      []
+
+doMoveC :: CBoard -> Mv -> Color -> CBoard 
+doMoveC board GiveUp  color = board
+doMoveC board Pass    color = board
+doMoveC board@(CBoard bl wh) (M i j) color =       
+    let ms = (i,j) : flippableIndicesC board color (i,j)
+        val = runST $ do
+           st <- newSTRef $ 0 :: ST s (STRef s Int64)
+           forM_ ms $ \(ii,jj) -> do
+             modifySTRef st ((.|.) (1 `shiftL` (ii + 8 * jj - 9)))
+           readSTRef st
+      in
+       if color == black
+         then CBoard (bl .|. val) (wh .|. complement val)
+         else CBoard (bl .|. complement val) (wh .|. val)
+
+-- valid moves (CBoard)
+validMovesC :: CBoard -> Color ->  [ (Int,Int) ]
+validMovesC board color =
+     filter (isValidMoveC board color) 
+             [ (i,j) | i <- [1..8], j <- [1..8]]
+
+countC :: CBoard -> Color -> Int 
+countC (CBoard bl wh) color 
+  | color == black = popCount bl
+  | color == white = popCount wh
+  | otherwise      = error $ "invalid color:" ++ show color
 showCBoard :: CBoard -> String
 showCBoard board = 
     " |A B C D E F G H \n" ++
