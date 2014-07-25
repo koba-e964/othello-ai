41dc534c4416cb7553ea444adf0f6765964887eb
koba-e964 <koba-e964@kobae964-Latitude-E5410.(none)>
2014-07-25 23:00:56 +0900

    Add new type (CBoard)
    CBoard represents a board with 2 Int64's.
    implemented init, readCBoard, and show.

--- Play.hs
+++ Play.hs
@@ -1,11 +1,14 @@
 module Play where 
 
+import Data.Bits
+import Data.Int
 import Data.Array.IO
 import Data.Array.MArray
 import Data.List ((\\))
 import Control.Monad 
 
 import System.Random
+import Text.Printf
 
 import Color 
 import Command
@@ -13,6 +16,51 @@ import Command
 
 -- 番兵付きの10x10配列
 type Board = IOUArray (Int,Int) Int 
+
+-- | each bit corresponds to a cell in the board:
+-- |  0  1  2  3  4  5  6  7
+-- |  8  9 10 11 12 13 14 15
+-- | ...
+-- | 56 57 58 59 60 61 62 63
+-- | (i,j) <====> 8*(j-1)+(i-1)
+-- | the first Int64 represents the positions of black disks.
+-- | the second Int64 represents the positions of white disks.
+data CBoard = CBoard !Int64 !Int64
+
+-- | (4,4) (27) and (5,5) (36) are white
+-- | (4,5) (28) and (5,4) (35) are black
+initCBoard :: CBoard
+initCBoard = CBoard (2^27 + 2^36) (2^28 + 2^35)
+
+readCBoard :: CBoard -> Int -> Int -> Color
+readCBoard board i j
+  | i <= 0 || i >= 9 || j <= 0 || j >= 9 = error $ printf "out of board: (%d, %d)" i j
+  | otherwise                            = readCBoardUnsafe board i j
+
+readCBoardUnsafe :: CBoard -> Int -> Int -> Color
+readCBoardUnsafe board@(CBoard bl wh) i j =
+  let ind = 8 * j + i - 9
+      mask= 1 `shiftL` ind :: Int64
+      bbit = bl .&. mask
+      wbit = wh .&. mask
+    in
+     if bbit /= 0 then black else if wbit /= 0 then white else none
+  
+showCBoard :: CBoard -> String
+showCBoard board = 
+    " |A B C D E F G H \n" ++
+      "-+----------------\n" ++
+       concatMap putBoardLine [1..8] ++
+        "  (X: Black,  O: White)"
+    where
+      putC c | c == none  = " " 
+             | c == white = "O"
+             | c == black = "X"
+      putBoardLine j =
+          show j ++ "|" ++
+           concatMap (\i -> let e = readCBoardUnsafe board i j in 
+                             putC e ++ " ") [1..8]
+             ++ "\n"
 
 initBoard :: IO Board 
 initBoard = 
