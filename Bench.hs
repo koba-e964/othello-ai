7eaf701c1e3a0e08f03aab536dad8eb0b049472f
koba-e964 <koba-e964@kobae964-Latitude-E5410.(none)>
2014-07-31 18:05:09 +0900

    Create testbench
    Bench.hs

---
+++ Bench.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Common
+import CBoard
+import AI
+import Control.Monad
+import Data.Bits
+import System.CPUTime
+import System.Environment
+import System.Random
+import Text.Printf
+
+test :: Places -> Int
+test param = eval5 param ((param * 0x123456789) &&& (complement param))
+
+bench :: Int -> IO Integer
+bench times = do
+  start <- getCPUTime
+  replicateM_ times $ do
+    param <- randomIO :: IO Places
+    let x = test param
+    return $! x
+  end <- getCPUTime
+  return $ end - start
+main :: IO ()
+main = do
+  args <- getArgs
+  let times = if null args then 1000000 else read (head args)
+  result <- bench times
+  printf "#iteration : %d, time : %d us\n" times (result `div` (10^(6 :: Int))) :: IO ()
+
--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 TARGETC=my-reversi
+BENCHC =bench
 SRCS=Common.hs Board.hs CBoard.hs Command.hs AI.hs Exam.hs
 
 all: $(TARGETS) $(TARGETC)
@@ -6,11 +7,13 @@ all: $(TARGETS) $(TARGETC)
 $(TARGETC): Client.hs $(SRCS)
 	ghc -O3 --make -Wall -o $(TARGETC) Client.hs 
 
-
+$(BENCHC) : Bench.hs $(SRCS)
+	ghc -O3 --make -Wall -o bench Bench.hs
 clean: 
 	rm -f *.o
 	rm -f *.hi
-	rm -f $(TARGETS)
-	rm -f $(TARGETS).exe
 	rm -f $(TARGETC)
 	rm -f $(TARGETC).exe
+	rm -f $(BENCHC)
+	rm -f $(BENCHC).exe
+
